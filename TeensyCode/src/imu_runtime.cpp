b5792158f052d0e068a0b8b9bf1b9bf814328d24
William Redenbaugh <51929010+william-redenbaugh@users.noreply.github.com>
2020-11-28 20:19:21 -0800

    Update imu_runtime.cpp
    Fixed adjustements for imu runtime

--- TeensyCode/src/imu_runtime.cpp
+++ TeensyCode/src/imu_runtime.cpp
@@ -24,17 +24,33 @@ void setup_imu_runtime(void){
 static void imu_thread(void *parameters){
 
     init_mpu6050(MPU6050_DEFAULT_I2C_ADDR, ACCELEROMETER_16G, GYRO_500_DEGREE_SECCOND); 
+
     for(;;){
-        os_thread_sleep_ms(100);       
+        os_thread_sleep_ms(10);
+
+        int n = millis();        
         // We get 20 samples of data. 
-        imu_data_raw lat_data = get_latest_mpu6050_data_sampled(20);     
+        imu_data_raw lat_data = get_latest_mpu6050_data_sampled(20);    
+
+        Serial.println((millis() - n));  
         accel_data_g dat = translate_accel_raw_g(lat_data); 
-        
-        if(dat.a_z < -.1)
-            trigger_led_strip_bike_animation(BIKE_LED_SIGNAL_STOP);
-        else if(dat.a_z < -.2)
-            trigger_led_strip_bike_animation(BIKE_LED_SIGNAL_STOP_FAST); 
+
+        bike_led_signal_state_t state = current_matrix_bike_animation();
+
+        if(dat.a_z < -.1){
+            if(dat.a_z >= -.3){
+                if(state != BIKE_LED_SIGNAL_STOP){
+                    trigger_led_strip_bike_animation(BIKE_LED_SIGNAL_STOP);
+                }
+            }
+            else{
+                if(state != BIKE_LED_SIGNAL_STOP_FAST){
+                    trigger_led_strip_bike_animation(BIKE_LED_SIGNAL_STOP_FAST); 
+                }
+            }
+        }
         else
-            trigger_led_strip_bike_animation(BIKE_LED_SIGNAL_WHITE);
+            if(state != BIKE_LED_SIGNAL_WHITE)
+                trigger_led_strip_bike_animation(BIKE_LED_SIGNAL_WHITE);
     }
 }
